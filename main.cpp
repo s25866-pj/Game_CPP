0a1b714e2e69a51d23c74eb5e383efd48269fa2b
s25866 <s25866@pjwstk.edu.pl>
2024-04-06 10:25:33 +0200

    add imgae

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 
 int main(int argc, char* args[] ) {
-    std::cout<<"program sie odpala";
-    int imgW = 48,imgH = 48;
     int speed = 5;
     SDL_Renderer* renderer;
     SDL_Event event;
@@ -13,29 +11,13 @@ int main(int argc, char* args[] ) {
     std::vector<SDL_Texture*> fragments;
     const int WindowWidth = 640, WindowHeight = 480;
     int x=0,y=0;
-    SDL_Rect rect={x,y,100,100};
-    SDL_Surface* surface = IMG_Load("kiwi_sprite.jpg");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    IMG_Init(IMG_INIT_PNG);
+    SDL_Surface* surface = IMG_Load("kiwi_sprite2.png");
     if(!surface){
         std::cout<<"nie mozna wczytac obrazka"<<std::endl;
         return 1;
     }
-
-    std::cout<<"zaladownao obrazek";
-    std::cout<<surface->h;
-    std::cout<<surface->w;
-    for(int i=0;i<surface->h;y+=imgH){
-        for(int j=0;j<surface->w;j+=imgW){
-            SDL_Rect imgRect ={i,j,imgW,imgH};
-            SDL_Surface* fragmentSurface = SDL_CreateRGBSurface(0,imgW,imgH,32,0,0,0,0);
-            SDL_BlitSurface(surface,&imgRect,fragmentSurface,NULL);
-            SDL_Texture* fragmentTexture = SDL_CreateTextureFromSurface(renderer,fragmentSurface);
-            fragments.push_back(fragmentTexture);
-            SDL_FreeSurface(fragmentSurface);
-//            std::cout<<"fragment obrazka wczytany";
-        }
-    }
-
-    SDL_Init( SDL_INIT_EVERYTHING );
     auto window = SDL_CreateWindow( "Moja Gra w CPP",
                                     SDL_WINDOWPOS_UNDEFINED,
                                     SDL_WINDOWPOS_UNDEFINED,
@@ -45,6 +27,8 @@ int main(int argc, char* args[] ) {
 
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
+    SDL_Rect rect={x,y,100,100};
+
     while(!quit){
         while(SDL_PollEvent(&event)){
             switch (event.type) { // Zmieniono warunek na event.type
@@ -87,9 +71,8 @@ int main(int argc, char* args[] ) {
 
         SDL_SetRenderDrawColor(renderer, 242, 5, 191, 255);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, fragments.at(5), NULL, &rect);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
         SDL_RenderPresent(renderer);
-
         SDL_Delay(25);
     }
     std::cout << "Dziękuję za grę";
