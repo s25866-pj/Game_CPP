9e8ba8aabf7a6f39ff1e1391139aefcdfac3b152
damian <s25866@pjwstk.edu.pl>
2024-05-14 00:24:42 +0200

    change image

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/damian/CLionProjects/Game_CPP
+# Install script for directory: /home/damian/Pulpit/CPP/Game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/damian/CLionProjects/Game_CPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/damian/Pulpit/CPP/Game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <SDL2/SDL_image.h>
-#include <vector>
 
 using namespace std;
 
@@ -11,39 +10,119 @@ public:
     int pos_Y = 0;
     int width = 100;
     int height = 100;
-    const char* image = "kiwi_sprite2.png";
-    int velocityY = 0;
-    double gravity = 1.0;
-    int jumpHeight = 300;
-};
-
-int main(int argc, char* args[]) {
-    Player player;
-    cout << player.width << endl;
     int speed = 5;
-    SDL_Renderer* renderer;
-    SDL_Event event;
-    bool imageLookingLeft = false;
-    bool quit = false;
-    std::vector<SDL_Texture*> fragments;
-    const int WindowWidth = 640, WindowHeight = 480;
-    int x = 0, y = 0;
-    SDL_Init(SDL_INIT_EVERYTHING);
+    bool left = false, right = false, moving = false;
+    const char* image = "player_sprites.png";
+    float airSpeed = 0;
+    float gravity = 0.4;
+    float jumpSpeed = -2.25;
+    float fallSpeedAfterCollision = 0.5;
+    bool inAir = false;
+
+    void handleInput(SDL_Keycode sym, bool keyPressed) {
+        if (keyPressed) {
+            if (sym == SDLK_a) {
+                left = true;
+            }
+            if (sym == SDLK_d) {
+                right = true;
+            }
+        } else {
+            if (sym == SDLK_a) {
+                left = false;
+            }
+            if (sym == SDLK_d) {
+                right = false;
+            }
+        }
+
+        // Update moving state based on input
+        moving = left || right;
+    }
+
+    void updatePosition(const int WindowWidth, const int WindowHeight) {
+        // Update Y position for gravity or jumping
+
+        if (inAir) {
+            airSpeed += gravity;
+            pos_Y += airSpeed;
+        } else {
+            airSpeed = 0;  // Reset air speed if on the ground
+        }
+
+        // Update X position based on movement
+        if (left && !right) {
+            pos_X -= speed;
+        } else if (!left && right) {
+            pos_X += speed;
+        }
+    }
+};
+SDL_Surface* importIMG(Player& player) {
     IMG_Init(IMG_INIT_PNG);
     SDL_Surface* surface = IMG_Load(player.image);
     if (!surface) {
-        std::cout << "Nie można wczytać obrazka" << std::endl;
+        std::cerr << "Nie można wczytać obrazka: " << IMG_GetError() << std::endl;
+    }
+    return surface;
+}
+SDL_Surface* loadAnimations(Player& player) {
+    SDL_Surface* surface = importIMG(player);
+    SDL_Surface* oneImage[5];
+
+    for (int i = 0; i < 5; ++i) {
+        oneImage[i]=SDL_CreateRGBSurface(0, 64, 40, 32, 0, 0, 0, 0);
+        SDL_Rect srcRect = { i * 64, 0, 40, 40 };
+        SDL_Rect destRect = { 0, 0, 64, 40 };
+        SDL_BlitSurface(surface, &srcRect, oneImage[i], &destRect);
+    }
+    return oneImage[0];
+}
+
+
+
+int main(int argc, char* args[]) {
+    Player player;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow("Moja Gra w CPP",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Nie można utworzyć okna: " << SDL_GetError() << std::endl;
         return 1;
     }
-    auto window = SDL_CreateWindow("Moja Gra w CPP",
-                                   SDL_WINDOWPOS_UNDEFINED,
-                                   SDL_WINDOWPOS_UNDEFINED,
-                                   WindowWidth, WindowHeight,
-                                   SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Nie można utworzyć renderera: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface* surface = loadAnimations(player);
+    if (!surface) {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
+    if (!texture) {
+        std::cerr << "Nie można utworzyć tekstury: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Rect rect = { player.pos_X, player.pos_Y, player.width, player.height };
+
+    SDL_Event event;
+    bool quit = false;
 
     while (!quit) {
         while (SDL_PollEvent(&event)) {
@@ -52,53 +131,16 @@ int main(int argc, char* args[]) {
                     quit = true;
                     break;
                 case SDL_KEYDOWN:
-                    switch (event.key.keysym.sym) {
-                        case SDLK_ESCAPE:
-                            quit = true;
-                            break;
-                        case SDLK_w:
-                            if(y<WindowHeight){
-                                x+=speed;
-                                rect.x=x;
-                            }
-                            break;
-                        case SDLK_a:
-                            if (x > 0) {
-                                x -= speed;
-                                rect.x = x;
-                            }
-                            break;
-                        case SDLK_s:
-                            if (y < WindowHeight - 100) {
-                                y += speed;
-                                rect.y = y;
-                            }
-                            break;
-                        case SDLK_d:
-                            if (x < WindowWidth - 100) {
-                                x += speed;
-                                rect.x = x;
-                            }
-                            break;
-                        case SDLK_SPACE:
-                            player.pos_X=300;
-                            cout<<"skok"<<endl;
-                            break;
-                    }
+                case SDL_KEYUP:
+                    player.handleInput(event.key.keysym.sym, event.type == SDL_KEYDOWN);
+                    player.updatePosition(640, 480); // Pass window size here
                     break;
             }
         }
-        if (player.pos_Y < WindowHeight - (player.height)) {
-            player.velocityY += player.gravity;
-            player.pos_Y += player.velocityY;
-            rect.y = player.pos_Y;
-        } else {
-            player.velocityY = 0;
 
-        }
-        if(player.gravity<1.0){
-            player.gravity+=0.2;
-        }
+        rect.x = player.pos_X;
+        rect.y = player.pos_Y;
+
         SDL_SetRenderDrawColor(renderer, 242, 5, 191, 255);
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, NULL, &rect);
@@ -108,6 +150,7 @@ int main(int argc, char* args[]) {
 
     std::cout << "Dziękuję za grę";
     SDL_Delay(1000);
+
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
